6f963e4a73a2e1b79243ab8614f00b4f6eca6848
Promedia <promediacitra0@gmail.com>
2024-07-04 22:22:06 +0700

    fix: add group_id in kategori.keterangan

--- addons/master_kategori_penilaian/models/kategori_keterangan.py
+++ addons/master_kategori_penilaian/models/kategori_keterangan.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class KategoriKeterangan(models.Model):
     _name = 'kategori.keterangan'
@@ -8,3 +8,10 @@ class KategoriKeterangan(models.Model):
         string="Keterangan"
     )
     jenis_id = fields.Many2one('kategori.jenis', string='Jenis', ondelete='cascade')
+    group_id = fields.Many2one('kategori.group', string='Grup', ondelete='cascade')
+
+    @api.onchange('keterangan')
+    def _compute_group_id(self):
+        for rec in self:
+            print(rec)
+            rec.group_id = rec.jenis_id.group_id
